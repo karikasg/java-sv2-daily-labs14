4ffbae998a6a52a3d9a5442de16435acd986b819
karikasg <karikas.gabor@gmail.com>
2022-01-24 17:08:13 +0100

    week14 day01 feladat

--- src/main/java/day01/OrderService.java
+++ src/main/java/day01/OrderService.java
@@ -16,6 +16,29 @@ public class OrderService {
         orders.add(order);
     }
 
+    public List<Order> findOrdersByStatus(String status) {
+        return orders.stream().filter(o -> o.getStatus().equals(status)).collect(Collectors.toList());
+    }
+
+    public long numbersOfOrderByStatus (String status) {
+        return orders.stream().filter(o -> o.getStatus().equals(status)).count();
+    }
+
+    public List<Order> getOrdersBetweenDates (LocalDate date1, LocalDate date2) {
+        return orders.stream().filter(o -> o.getOrderDate().isAfter(date1.minusDays(1)) && o.getOrderDate().isBefore(date2.plusDays(1))).collect(Collectors.toList());
+    }
+
+    public boolean orderFewerThan (int piece) {
+        return orders.stream().anyMatch(o -> o.getProducts().size() < piece);
+    }
+
+    public Order orderMax () {
+        return orders.stream().max(Comparator.comparing(o -> o.getProducts().size())).get();
+    }
+
+    public List<Order> orderFindByCategory (String category) {
+        return orders.stream().filter(o -> o.getProducts().stream().anyMatch(x -> x.getCategory().equals(category))).collect(Collectors.toList());
+    }
 
 
 }
--- src/test/java/day01/OrderServiceTest.java
+++ src/test/java/day01/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package day01;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
@@ -33,6 +34,7 @@ class OrderServiceTest {
         Order o3 = new Order("pending", LocalDate.of(2021, 06, 07));
         o3.addProduct(p1);
         o3.addProduct(p2);
+        o3.addProduct(p3);
         o3.addProduct(p5);
 
         Order o4 = new Order("on delivery", LocalDate.of(2021, 06, 01));
@@ -41,8 +43,8 @@ class OrderServiceTest {
         o4.addProduct(p2);
 
         Order o5 = new Order("pending", LocalDate.of(2021, 06, 07));
-        o5.addProduct(p1);
-        o5.addProduct(p2);
+//        o5.addProduct(p1);
+//        o5.addProduct(p2);
         o5.addProduct(p5);
 
         orderService.saveOrder(o1);
@@ -52,4 +54,38 @@ class OrderServiceTest {
         orderService.saveOrder(o5);
     }
 
+    @Test
+    void getOrdersByStatusTest() {
+    }
+
+    @Test
+    void numbersOfOrderByStatusTest() {
+        assertEquals(3, orderService.numbersOfOrderByStatus("pending"));
+        assertEquals(2, orderService.numbersOfOrderByStatus("on delivery"));
+    }
+
+    @Test
+    void getOrdersBetweenDatesTest() {
+        assertEquals(3, orderService.getOrdersBetweenDates(LocalDate.of(2021, 6, 5), LocalDate.of(2021, 6, 7)).size());
+        assertEquals(5, orderService.getOrdersBetweenDates(LocalDate.of(2021, 6, 1), LocalDate.of(2021, 6, 10)).size());
+        assertEquals(2, orderService.getOrdersBetweenDates(LocalDate.of(2021, 6, 1), LocalDate.of(2021, 6, 1)).size());
+    }
+
+    @Test
+    void orderFewerThanTest() {
+        assertTrue(orderService.orderFewerThan(4));
+        assertTrue(orderService.orderFewerThan(3));
+        assertFalse(orderService.orderFewerThan(2));
+    }
+
+    @Test
+    void orderMax() {
+        assertEquals(4, orderService.orderMax().getProducts().size());
+    }
+
+    @Test
+    void orderFindByCategoryTest() {
+        assertEquals(3, orderService.orderFindByCategory("Book").size());
+        assertEquals(4, orderService.orderFindByCategory("IT").size());
+    }
 }
